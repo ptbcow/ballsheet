6e9a5a7b6dadeab6c5d3a8f32558e43797c44037
dphdmn <58388093+dphdmn@users.noreply.github.com>
2020-12-04 04:21:17 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -387,7 +387,7 @@ function textThings()
    text("BallSheet",W/2,H-H/12);
    textSize(25);
    fill(250,150,200, alpha);
-   text("v6.1 for ian",W/2,H-H/25);
+   text("v6.2 for ian",W/2,H-H/25);
    
    fill(200-balance,balance*3+150,0, alpha);
    text(balance.toFixed(0),W/2,H-H/7);
